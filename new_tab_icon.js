1d332877590c198d18b13c0a81f96a93176062fd
migmanu <jmanuelmigoya@gmail.com>
2022-03-16 19:59:51 -0300

    Modify html returned on right click start displaying icon edit box form

--- new_tab_icon.js
+++ new_tab_icon.js
@@ -1,13 +1,22 @@
 /* listens for secondary click on icons and siplays menu to edit links and images */
 
-let els = document.getElementsByClassName("btn")
+let els = document.getElementsByClassName("btnForm")
 let el = document.querySelector(".btn");
 
 function modLink() {
+    event.preventDefault();
     let inputBox = `
-    <p class="link-box">Test listener</p>
+    <div class="edit-form">
+        <form>
+            <h2>Edit Icon</h2>
+            <input type="text" name="Website Title" maxlength="14" required>
+            <input type="url" name="Website URL" required>
+            <input type="file" name="Icon" accept="image/png, image/jpeg">
+            <input type="submit" name="Submit form">
+        </form>
+    </div>
     `
-    el.insertAdjacentHTML("beforebegin", inputBox);
+    this.insertAdjacentHTML("afterbegin", inputBox);
 }
 
 Array.from(els).forEach(function(e) {
