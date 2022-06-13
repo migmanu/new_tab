0dec2fb89750c63f13ba8c66557714d58214c537
migmanu <jmanuelmigoya@gmail.com>
2022-06-13 20:07:36 +0200

    delete icon constructor

--- JS/index.js
+++ JS/index.js
@@ -3,29 +3,13 @@ Component made to control icons behavior. Searches all elements with "btnForm" c
 adds event listeners for right click and changes the icon edit form display property
  edit menu when event fires.
 */
-chrome.storage.local.set({icon1: {website_title: "third example"}})
+chrome.storage.local.set({icon1: {website_title: "first icon", website_URL : "", website_Image: ""}})
 chrome.storage.local.set({icon2: {website_title: "wonderfull!"}})
 
                         
 let iconToEdit = ''
 
 const iconFormsList = document.getElementsByClassName("btnForm");
-
-
-// construct each icon retrieving its data from Chrome local storage
-Array.from(iconFormsList).forEach(element => {
-    const elementID = element.id
-    const iconToBuild = document.getElementById(elementID)
-    console.log(`element id: ${elementID}`);
-
-    let elementTitle
-    chrome.storage.local.get([`${elementID}`], function (result) {
-        console.log("chrome API init")
-        elementTitle = result[`${elementID}`].website_title
-        console.log(`element title is: ${elementTitle} type: ${typeof(elementTitle)}`)
-        document.getElementById(`${elementID}-btn-legend`).innerText = elementTitle
-    })
-})
 
 
 Array.from(iconFormsList).forEach(element => {
