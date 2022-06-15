f0bafbb2a54262c484be38d4dcef217d2e483c6a
migmanu <jmanuelmigoya@gmail.com>
2022-06-15 12:59:48 +0200

    init variables and chrome API call

--- JS/icons.js
+++ JS/icons.js
@@ -4,6 +4,16 @@ for (let n = 0; n < 8; n++) {
   const iconName = 'icon' + n
   let iconRow = document.getElementById("first-icon-row-container")
   
+  let iconTitle
+  let iconURL
+  let iconImage
+  chrome.storage.local.get([iconName], function(result) {
+    if (result[`${iconName}`]) {
+      console.log(`icon name is ${result[`${iconName}`].website_title}`)
+      let iconTitle = result[`${iconName}`].website_title
+    }
+  })
+
   if (n > 3) {
     iconRow = document.getElementById("second-icon-row-container")
   }
@@ -13,7 +23,7 @@ for (let n = 0; n < 8; n++) {
                             <img class="btn-image" src="./Icons/gmail.ico" alt="gmail icon">
                             <span class="sr-only">Link to favorite site</span>
                         </button>
-                        <p id="${iconName}-btn-legend" class="btn-legend"></p>
+                        <p id="${iconName}-btn-legend" class="btn-legend">${iconTitle}</p>
                     </form>`
 
 
