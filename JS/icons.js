613c79d5026133f00010a02f933ca2af5550886e
migmanu <jmanuelmigoya@gmail.com>
2022-06-15 13:32:12 +0200

    attempt to build iconsObject

--- JS/icons.js
+++ JS/icons.js
@@ -1,18 +1,36 @@
 // Script meant to build all eight icons in two rows of four
 
+let iconsObject = {}
+
+for (let n = 0; n < 8; n++) {
+  const iconName = 'icon' + n;
+
+  let iconTitle = chrome.storage.local.get([iconName], function (result) {
+    console.log("icons object API call init")
+    
+  })
+}
+
+
 for (let n = 0; n < 8; n++) {
   const iconName = 'icon' + n
   let iconRow = document.getElementById("first-icon-row-container")
   
-  let iconTitle
-  let iconURL
+  let iconURL = "test"
   let iconImage
-  chrome.storage.local.get([iconName], function(result) {
+  let iconTitle = chrome.storage.local.get([iconName], (result) => {
+    console.log(`icon url: ${iconURL}`)
+    iconURL = "second test"
+    console.log(`second icon url: ${iconURL}`)
     if (result[`${iconName}`]) {
       console.log(`icon name is ${result[`${iconName}`].website_title}`)
       let iconTitle = result[`${iconName}`].website_title
+      console.log(`icon title var is: ${iconTitle}`)
     }
+  console.log(`icon title before return: ${iconTitle}`)
+  return iconTitle
   })
+  console.log(`outside icon title var is: ${iconTitle}`)
 
   if (n > 3) {
     iconRow = document.getElementById("second-icon-row-container")
